89e94a4e60001de4dbb20d0d581648e197a3124c
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-02-21 17:53:14 -0500

    flattened fb

--- support.mjs
+++ support.mjs
@@ -2,28 +2,18 @@
 import { decode } from 'html-entities';
 
 
-let state_names = [];
-
 function legalize (s) {
     return s.replace (/ /g, "_");
-}
-
-function memo_state_name (s) {
-    state_names.push (s);
-    return "";
-}
-
-function create_stepper () {
-    let result = "\n\n❴{\n";
-    state_names.forEach (name => {
-	result += `\n"${name}": ${legalize (name)},`;
-    });
-    result += "}\n❵ [self.state] ()\n";
-    return result;
 }
 
 function decodeHTML(s) {
     let prev;
     do { prev = s; s = decode(s); } while (s !== prev);
     return s;
+}
+
+let id = 0;
+function createid () {
+    id += 1;
+    return `${id}`;
 }
